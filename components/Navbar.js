bc8824a18845910995eb13412cc790487cf8ea99
John Doe <johndoe@email.com>
2022-10-13 23:32:42 +0530

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -126,13 +126,13 @@ function NavBar() {
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
               <img
-                className="block lg:hidden h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500"
+                className="block lg:hidden h-10 w-auto"
+                src="stylvee_logo.jpeg"
                 alt="Workflow"
               />
               <img
-                className="hidden lg:block h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500"
+                className="hidden lg:block h-10 w-auto"
+                src="stylvee_logo.jpeg"
                 alt="Workflow"
                 onClick={()=>router.push('/')}
               />
